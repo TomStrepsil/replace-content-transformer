21e255bddbd28063a9b4766af8482901d1d71a31
Tom Pereira <10725179+TomStrepsil@users.noreply.github.com>
2026-05-09 13:19:03 +0100

    remove redundant re-export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export * from "./search-strategies/index.js";
 export * from "./search-strategy-factory.js";
-export * from "./engines/async-lookahead-transform-engine/index.js";
 export * from "./engines/index.js";
